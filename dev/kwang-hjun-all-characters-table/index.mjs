10f9143f523d647cea6fc7db5ce08e3fe3603546
Nianyi Wang <wangnianyi2001@outlook.com>
2024-06-02 22:34:19 +0800

    Extract and refactor the scripts so that the toolkit functions are not coupled to certain dictionary

--- dev/kwang-hjun-all-characters-table/index.mjs
+++ dev/kwang-hjun-all-characters-table/index.mjs
@@ -1,2 +1,19 @@
-export { glyphs } from './data.mjs';
-export * from './toolkit.mjs';
+import * as DataLoader from './data-loader.mjs';
+
+/**
+ * @typedef {import('./data-loader.mjs').KwangHjunAllCharactersTableRow} KwangHjunAllCharactersTableRow
+ * @typedef {Map<string, KwangHjunAllCharactersTableRow[]>} GlyphRecords
+ */
+
+const table = await DataLoader.LoadAsync();
+/** @type {GlyphRecords} */
+const glyphs = new Map();
+for(const row of table) {
+	const glyph = row.guangYunZiTou_HeJiaoHou;
+	if(!glyphs.has(glyph))
+		glyphs.set(glyph, []);
+	const entries = glyphs.get(glyph);
+	entries.push(row);
+}
+
+export default glyphs;
--- dev/main.mjs
+++ dev/main.mjs
@@ -1,8 +1,8 @@
-import * as KwangHjun from './kwang-hjun-all-characters-table/index.mjs';
+import * as HanChem from './index.mjs';
 
 async function Main() {
-	const targetShapePart = '亼';
-	const containingGlyphs = KwangHjun.FindAllGlyphsContainingShapePart(targetShapePart);
+	const targetShapePart = '僉';
+	const containingGlyphs = HanChem.FindAllGlyphsContainingShapePart(targetShapePart);
 
 	console.log(`所有（直接或间接）包含"${targetShapePart}"的字形：`);
 	console.log(`${Array.from(containingGlyphs.values()).join('')}`);
